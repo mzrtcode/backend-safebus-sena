bfd6575e64221f0f910755de2f475559a2f0c105
Stiven Medina <stvnm33@gmail.com>
2023-07-13 15:12:55 -0500

    Arreglando JWT

--- src/libs/jwt.js
+++ src/libs/jwt.js
@@ -5,7 +5,7 @@ export function crearTokenAcceso(payload){
     return new Promise((resolve, reject) => {
         jwt.sign(
             payload
-        , TOKEN_SECRET ,{},
+        , TOKEN_SECRETO ,{},
         (err, token) => {
             if(err) reject(err)
             resolve(token)
