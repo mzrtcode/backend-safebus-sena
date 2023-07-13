8e640d158adea1ccd1d697b3cd1033afa7fe2e12
Stiven Medina <stvnm33@gmail.com>
2023-07-13 15:25:35 -0500

    Añadiendo middleware para validar el token

---
+++ src/middlewares/validarToken.middleware.js
@@ -0,0 +1,13 @@
+import jwt from 'jsonwebtoken';
+const TOKEN_SECRETO = process.env.TOKEN_SECRET;
+
+export const authRequired = (req, res, next) => {
+    const {token} = req.cookies;
+
+    if(!token) return res.status(401).json({message: 'No token, No autorizado'});
+    jwt.verify(token, TOKEN_SECRETO, (err, decoded) => {
+        if(err) return res.status(401).json({message: 'Token invalido'});
+        req.user = decoded;
+    })
+    next();
+}
