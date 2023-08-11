ca71ff128b82b4710a451907ccd86f33206bbb81
Stiven Medina <stvnm33@gmail.com>
2023-08-10 21:31:53 -0500

    Arreglando el controlador de Conductores CRUD completo implementado

--- src/controllers/conductores.controller.js
+++ src/controllers/conductores.controller.js
@@ -10,3 +10,82 @@ export const obtenerConductores = async (req, res) => {
     res.status(500).json({ mensaje: 'Error al obtener las conductores' })
   }
 }
+
+export const crearConductor = async (req, res) => {
+  try {
+    const { nombres, apellidos, tipo_identificacion, numero_identificacion, correo, celular, fecha_nacimiento, direccion } = req.body
+
+    const [resultado] = await pool.query(
+      'INSERT INTO conductores (nombres, apellidos, tipo_identificacion, numero_identificacion, correo, celular, fecha_nacimiento, direccion) VALUES (?, ?, ?, ?, ?, ?, ?, ?)',
+      [nombres, apellidos, tipo_identificacion, numero_identificacion, correo, celular, fecha_nacimiento, direccion]
+    )
+    const conductorCreado = {
+      id: resultado.insertId, // Obtener el ID generado por la base de datos
+      nombres,
+      apellidos,
+      tipo_identificacion,
+      numero_identificacion,
+      correo,
+      celular,
+      fecha_nacimiento,
+      direccion
+    }
+
+    res.status(201).json(conductorCreado)
+  } catch (error) {
+    console.log(error)
+    res.status(500).json({ mensaje: 'Error al crear el conductor' })
+  }
+}
+
+export const obtenerConductor = async (req, res) => {
+  try {
+    const { id } = req.params
+    const [resultado] = await pool.query('SELECT * FROM conductores WHERE id_conductor = ?', [id])
+
+    if (resultado.length === 0) {
+      return res.status(404).json({ mensaje: 'No existe el conductor' })
+    }
+
+    res.status(200).json(resultado[0])
+  } catch (error) {
+    console.log(error)
+    res.status(500).json({ mensaje: 'Error al obtener el conductor' })
+  }
+}
+
+export const eliminarConductor = async (req, res) => {
+  try {
+    const { id } = req.params
+    const [resultado] = await pool.query('DELETE FROM conductores WHERE id_conductor = ?', [id])
+
+    if (resultado.affectedRows === 1) {
+      res.status(204).json({ mensaje: 'Conductor eliminada' })
+    } else {
+      res.status(404).json({ mensaje: 'Conductor no encontrada' })
+    }
+  } catch (error) {
+    console.error(error)
+    res.status(500).json({ mensaje: 'Error al eliminar el conductor' })
+  }
+}
+
+export const actualizarConductor = async (req, res) => {
+  const { id } = req.params
+  const { nombres, apellidos, tipo_identificacion, numero_identificacion, correo, celular, fecha_nacimiento, direccion } = req.body
+  try {
+    const [resultado] = await pool.query(
+      'UPDATE conductores SET nombres = IFNULL(?, nombres), apellidos = IFNULL(?, apellidos), tipo_identificacion = IFNULL(?, tipo_identificacion), numero_identificacion = IFNULL(?, numero_identificacion), correo = IFNULL(?, correo), celular = IFNULL(?, celular), fecha_nacimiento = IFNULL(?, fecha_nacimiento), direccion = IFNULL(?, direccion) WHERE id_ruta = ?',
+      [nombres, apellidos, tipo_identificacion, numero_identificacion, correo, celular, fecha_nacimiento, direccion, id]
+    )
+
+    if (resultado.affectedRows === 1) {
+      res.status(200).json({ mensaje: 'Conductor actualizada' })
+    } else {
+      res.status(404).json({ mensaje: 'Conductor no encontrada' })
+    }
+  } catch (error) {
+    console.error(error)
+    res.status(500).json({ mensaje: 'Error al actualizar la conductor', error: error.message })
+  }
+}
--- src/routes/conductores.routes.js
+++ src/routes/conductores.routes.js
@@ -1,8 +1,17 @@
 import { Router } from 'express'
-import { obtenerConductores } from '../controllers/conductores.controller.js'
+import { actualizarConductor, crearConductor, eliminarConductor, obtenerConductor, obtenerConductores } from '../controllers/conductores.controller.js'
+import { autenticacionRequerida } from '../middlewares/validarToken.middleware.js'
 
 const router = Router()
 
 router.get('/conductores', obtenerConductores)
+router.get('/conductores/:id', obtenerConductor)
+router.post(
+  '/conductores',
+  autenticacionRequerida, // Middleware para validar la autenticación requerida antes de crear una localidad.
+  crearConductor // Controlador de creación de localidad.
+)
+router.put('/conductores/:id', actualizarConductor) // Ruta para actualizar una localidad por su ID. Llama al controlador de actualización de localidad.
+router.delete('/conductores/:id', eliminarConductor) // Ruta para eliminar una localidad por su ID. Llama al controlador de eliminación de localidad. */
 
-export default router
+export default router // Se exporta la instancia de enrutador como el módulo predeterminado
