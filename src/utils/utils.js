cc9433a959c1f50bc9eac5fe4d9950e0521ebcb3
Stiven Medina <stvnm33@gmail.com>
2023-11-24 13:08:06 -0500

    Añadiendo funcion para calcular la hora de salida en utils

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -5,3 +5,29 @@ export function obtenerFechaHoraMySQL() {
     const formattedFechaHora = fechaHoraUTC.toISOString().slice(0, 19).replace('T', ' ');
     return formattedFechaHora;
   }
+
+/**
+ * Retorna la hora actual sumándole treinta minutos en formato timestamp para MySQL.
+ * @param {Date} fecha - Objeto Date al que se le sumarán los minutos.
+ * @returns {string} - String en formato timestamp para MySQL con la nueva hora.
+ */
+export function calcularHoraSalida(fecha) {
+  // Clonar la fecha para evitar modificar la original
+  const nuevaFecha = new Date(fecha);
+
+  // Sumar 30 minutos
+  nuevaFecha.setMinutes(nuevaFecha.getMinutes() + 30);
+
+  // Obtener los componentes de la fecha
+  const year = nuevaFecha.getFullYear();
+  const month = String(nuevaFecha.getMonth() + 1).padStart(2, '0');
+  const day = String(nuevaFecha.getDate()).padStart(2, '0');
+  const hours = String(nuevaFecha.getHours()).padStart(2, '0');
+  const minutes = String(nuevaFecha.getMinutes()).padStart(2, '0');
+  const seconds = String(nuevaFecha.getSeconds()).padStart(2, '0');
+
+  // Formatear la fecha como timestamp para MySQL
+  const timestamp = `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
+
+  return timestamp;
+}
