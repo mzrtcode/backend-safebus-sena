683e8322e0cd5a30e03176e96942d8de08e9c52e
Stiven Medina <stvnm33@gmail.com>
2023-11-26 14:53:58 -0500

    Añadiendo ruta para actualizar empresa tipo PUT

--- src/routes/empresa.routes.js
+++ src/routes/empresa.routes.js
@@ -1,11 +1,14 @@
 import { Router } from 'express'
-import { obtenerEmpresa } from '../controllers/empresa.controller.js'
-import { autenticacionRequerida } from '../middlewares/validarToken.middleware.js'
+import { actualizarEmpresa, obtenerEmpresa } from '../controllers/empresa.controller.js'
+import { autenticacionAdmin, autenticacionRequerida } from '../middlewares/validarToken.middleware.js'
+import { validarSchema } from '../middlewares/schemaValidator.middleware.js'
+import { actualizarEmpresaSchema } from '../schemas/empresa.schema.js'
 
 
 const router = Router()
 
 router.get('/empresa', autenticacionRequerida, obtenerEmpresa)
+router.put('/empresa', autenticacionRequerida, autenticacionAdmin, validarSchema(actualizarEmpresaSchema), actualizarEmpresa)
 
 
 export default router // Se exporta la instancia de enrutador como el módulo predeterminado
