7dc8b3e7519ce72575d939429fdddd2accb62317
Stiven Medina <stvnm33@gmail.com>
2023-11-29 20:17:44 -0500

    Añadiendo la nueva ruta para reportes

--- src/routes/reportes.routes.js
+++ src/routes/reportes.routes.js
@@ -1,11 +1,12 @@
 import { Router } from 'express'
 import {  autenticacionVendedor } from '../middlewares/validarToken.middleware.js'
-import { obtnerReportePlanillaje } from '../controllers/reportes.controller.js'
-import { validarSchema } from '../middlewares/schemaValidator.middleware.js'
-import { obtenerReportePlanillajeSchema } from '../schemas/reportes.schema.js'
+import { obtenerReportePlanillajeController, obtenerReporteRelacionPlanillasController } from '../controllers/reportes.controller.js'
+
 
 const router = Router()
 
-router.get('/reportes', autenticacionVendedor, validarSchema(obtenerReportePlanillajeSchema), obtnerReportePlanillaje)
+router.get('/reportes/resumen-pasajes', autenticacionVendedor, obtenerReportePlanillajeController)
+router.get('/reportes/relacion-planillas', autenticacionVendedor, obtenerReporteRelacionPlanillasController)
+
 
 export default router // Se exporta la instancia de enrutador como el módulo predeterminado
